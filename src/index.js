62a11069bc495958cbc44075da6f647783eba800
theartistboe <134174401+theartistboe@users.noreply.github.com>
2023-06-04 22:00:33 -0700

    updated status htmland h2 in css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 function displayTemperature(response) {
-    console.log(response.data.main.temp);
+    console.log(response.data);
     let tempElement = document.querySelector("#temp");
     let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#desciption")
     tempElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
+    descriptionElement = response.data.weather[0].description;
 }
 
 let apiKey = "4b3503b2f08a729413c4d33ef1186004"
