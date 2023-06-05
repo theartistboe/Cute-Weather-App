211edf45f72e9729007fe6a0b9263b2c783234f7
theartistboe <134174401+theartistboe@users.noreply.github.com>
2023-06-04 20:41:31 -0700

    add api key and api url to js file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-let apikey = "4b3503b2f08a729413c4d33ef1186004"
+let apiKey = "4b3503b2f08a729413c4d33ef1186004"
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Merced&appid=${apiKey}&units=metric`
+
+console.log(apiUrl)
