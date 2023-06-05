73dce6f1d6afd36561b8ad9343fbf4f1a205c5fa
theartistboe <134174401+theartistboe@users.noreply.github.com>
2023-06-04 20:45:50 -0700

    debugged axios.get api

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
+function displayTemperature(response) {
+    console.log(response.data.main.temp);
+}
+
 let apiKey = "4b3503b2f08a729413c4d33ef1186004"
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Merced&appid=${apiKey}&units=metric`
 
-console.log(apiUrl)
+axios.get(apiUrl).then(displayTemperature);
