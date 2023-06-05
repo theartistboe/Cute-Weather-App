3945927e10104922c57afba17a33e70c70b87f3d
theartistboe <134174401+theartistboe@users.noreply.github.com>
2023-06-04 22:51:39 -0700

    added wind and humidity elements

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ function displayTemperature(response) {
     let tempElement = document.querySelector("#temp");
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind")
     tempElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    windElement.innerHTML = response.data.wind.speed;
 }
+
 
 let apiKey = "4b3503b2f08a729413c4d33ef1186004"
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Merced&appid=${apiKey}&units=metric`
