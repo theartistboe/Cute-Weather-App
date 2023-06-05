b8fd08f1bf686256a0eaf0006a451b7379a37a56
theartistboe <134174401+theartistboe@users.noreply.github.com>
2023-06-04 21:11:42 -0700

    fixed to define response within function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 function displayTemperature(response) {
     console.log(response.data.main.temp);
+    let tempElement = document.querySelector("#temp");
+    tempElement.innerHTML = Math.round(response.data.main.temp);
 }
 
 let apiKey = "4b3503b2f08a729413c4d33ef1186004"
