f70b97de95cbfb15c7669af7ac3f7c2eb0fe2a58
theartistboe <134174401+theartistboe@users.noreply.github.com>
2023-06-11 16:21:00 -0700

    update html days and js current open weather icon

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
+
 function formatDate(timestamp) {
     let date = new Date(timestamp);
     let hours = date.getHours();
     if (hours < 10) {
-        hours = `0${hours}`;
+      hours = `0${hours}`;
     }
     let minutes = date.getMinutes();
     if (minutes < 10) {
-        minutes = `0${minutes}`;
+      minutes = `0${minutes}`;
     }
-    let days = ["Sunday", "Monday", "Tuesday", "Wedsnesday", "Thursday", "Friday", "Saturday"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     let day = days[date.getDay()];
-    return `${day} ${hours} ${minutes}`;
-}
-
-function displayTemperature(response) {
+    return `${day} ${hours}:${minutes}`;
+  }
+  
+  function displayTemperature(response) {
     console.log(response.data);
     let tempElement = document.querySelector("#temp");
     let cityElement = document.querySelector("#city");
@@ -28,10 +29,12 @@ function displayTemperature(response) {
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = response.data.wind.speed;
     datetimeElement.innerHTML = formatDate(response.data.dt * 1000);
-    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`);
-}
-
-let apiKey = "4b3503b2f08a729413c4d33ef1186004"
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Merced&appid=${apiKey}&units=metric`
-
-axios.get(apiUrl).then(displayTemperature);
+    let iconCode = response.data.weather[0].icon;
+    let iconUrl = `https://openweathermap.org/img/wn/${iconCode}.png`;
+    iconElement.setAttribute("src", iconUrl);
+  }
+  
+  let apiKey = "4b3503b2f08a729413c4d33ef1186004";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Merced&appid=${apiKey}&units=metric`;
+  
+  axios.get(apiUrl).then(displayTemperature);
