3343ce9b1f7e422a38f31d81315e2fe3c49bc836
theartistboe <134174401+theartistboe@users.noreply.github.com>
2023-06-11 13:58:36 -0700

    added days of the week array

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let days = ["Sunday", "Monday", "Tuesday", "Wedsnesday", "Thursday", "Friday", "Saturday"];
+    let day = date.getDay();
+    return `${day} ${hours} ${minutes}`;
+}
+
 function displayTemperature(response) {
     console.log(response.data);
     let tempElement = document.querySelector("#temp");
@@ -5,11 +14,13 @@ function displayTemperature(response) {
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind")
+    let datetimeElement = document.querySelector("#datetime")
     tempElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = response.data.wind.speed;
+    datetimeElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "4b3503b2f08a729413c4d33ef1186004"
