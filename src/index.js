17c4f5eb3a78ecf88cf2a6339b14b52e496787c2
theartistboe <134174401+theartistboe@users.noreply.github.com>
2023-06-11 13:30:17 -0700

    remove extra space

--- src/index.js
+++ src/index.js
@@ -12,7 +12,6 @@ function displayTemperature(response) {
     windElement.innerHTML = response.data.wind.speed;
 }
 
-
 let apiKey = "4b3503b2f08a729413c4d33ef1186004"
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Merced&appid=${apiKey}&units=metric`
 
