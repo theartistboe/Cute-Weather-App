5991c10e084b8ade282a1cb28280386cd545b44e
theartistboe <134174401+theartistboe@users.noreply.github.com>
2023-06-18 21:30:22 -0700

    Update index.js
    added weather forecast api for 6 days in js

--- src/index.js
+++ src/index.js
@@ -13,21 +13,30 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`;
   }
 
+  function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tues", "Weds", "Thurs", "Fri", "Sat"];
+
+    return days[day];
+  }
+
   function displayForecast(response) {
-    console.log(response.data.daily);
+    let forecast = response.data.daily;
     let forecastElement = document.querySelector("#forecast");
   
     let forecastHTML = `<div class="row">`;
-    let days = ["Sun", "Mon", "Tues", "Weds", "Thurs", "Fri"];
-    days.forEach(function (day) {
+    forecast.forEach(function (forecastDay, index) {
+      if (index < 6) { 
     forecastHTML = forecastHTML + `
       <div class="col-2">
-        <div class="weather-forecast-date">${day}</div>
-        <img src="https://openweathermap.org/img/wn/03d.png" alt="">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}.png" alt="">
         <br />
-        <span class="weather-forecast-temperature-max">37°F</span> | <span class="weather-forecast-temperature-min">32°C</span>
+        <span class="weather-forecast-temperature-max">${Math.round(forecastDay.temp.max)}°F</span> | <span class="weather-forecast-temperature-min">${Math.round(forecastDay.temp.min)}°C</span>
       </div>
     `;
+        }
       })
     forecastElement.innerHTML = forecastHTML;
   }
